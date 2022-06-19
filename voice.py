3d9efa398f6fd76eb1d6ba1a5af1e000be28920c
Abedur Rahman <abedurrahman03@gmail.com>
2022-06-19 09:29:34 +0600

    hi

--- voice.py
+++ voice.py
@@ -7,3 +7,19 @@ class Voice:
         print("     Reminder ( will give the reminder)")
         print("     Text (convert voice to text)")
         print("end of info *****************")
+
+
+import speech_recognition as sr
+import pyaudio
+
+r = sr.Recognizer()
+while True:
+    with sr.Microphone() as source:
+        print("Speak Anything :")
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            print("You said : {}".format(text))
+        except:
+            print("Sorry could not recognize what you said")
+
